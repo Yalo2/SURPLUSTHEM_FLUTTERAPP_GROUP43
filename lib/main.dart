79260182a27a90b4a00fb3509ac14129c7894ec1
HENS <ssenyondohenry644@gmail.com>
2026-07-09 18:58:01 +0300

    Set-up initial firestore setup!

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  print("Firebase initialized successfully");
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello, Firebase!', style: TextStyle(fontSize: 24)),
+        ),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
