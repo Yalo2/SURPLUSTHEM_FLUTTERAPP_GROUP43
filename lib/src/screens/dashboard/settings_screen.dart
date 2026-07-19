e51ba65e033c90b6c46b13082abbeeee1cf6f5b2
Yalo2 <122814840+Yalo2@users.noreply.github.com>
2026-07-20 00:40:54 +0300

    settiing screen

--- lib/src/screens/dashboard/settings_screen.dart
+++ lib/src/screens/dashboard/settings_screen.dart
@@ -1,1 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../theme/theme_controller.dart';
 
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
