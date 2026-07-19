7df16e0b4445aa70d26f731ccf10c40a61483e3b
Yalo2 <122814840+Yalo2@users.noreply.github.com>
2026-07-20 00:20:56 +0300

    profile screen

--- lib/src/screens/dashboard/profile_screen.dart
+++ lib/src/screens/dashboard/profile_screen.dart
@@ -1,1 +1,15 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
+import 'my_donations_screen.dart';
+import 'settings_screen.dart';
+import '../../services/cloudinary_service.dart';
 
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
