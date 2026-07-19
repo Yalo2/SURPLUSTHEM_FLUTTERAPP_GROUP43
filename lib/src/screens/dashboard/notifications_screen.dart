139e5af11bf7f8e16311c4e0e3af0ce61a58f800
Yalo2 <122814840+Yalo2@users.noreply.github.com>
2026-07-20 00:42:54 +0300

    nofi builder

--- lib/src/screens/dashboard/notifications_screen.dart
+++ lib/src/screens/dashboard/notifications_screen.dart
@@ -6,3 +6,30 @@ class NotificationsScreen extends StatelessWidget {
   const NotificationsScreen({super.key});
 
   @override
+
+    Widget build(BuildContext context) {
+    final userId = FirebaseAuth.instance.currentUser!.uid;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF2E7D32),
+        title: const Text('Notifications'),
+        foregroundColor: Colors.white,
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream:
+            FirebaseFirestore.instance
+                .collection('notifications')
+                .where('userId', isEqualTo: userId)
+                .orderBy('createdAt', descending: true)
+                .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final notifications = snapshot.data!.docs;
+
+          if (notifications.isEmpty) {
+            return const Center(child: Text('No notifications yet'));
+          }
