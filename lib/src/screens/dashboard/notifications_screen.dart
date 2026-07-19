4a0b5e6c64f3b54c430f38fc3809c67dae3c92ad
Yalo2 <122814840+Yalo2@users.noreply.github.com>
2026-07-20 00:42:26 +0300

    nofi screen

--- lib/src/screens/dashboard/notifications_screen.dart
+++ lib/src/screens/dashboard/notifications_screen.dart
@@ -1,1 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+class NotificationsScreen extends StatelessWidget {
+  const NotificationsScreen({super.key});
+
+  @override
