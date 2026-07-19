89739f9c84654f34361b55e20f81d96808eb8540
Yalo2 <122814840+Yalo2@users.noreply.github.com>
2026-07-20 00:45:21 +0300

    donation screen

--- lib/src/screens/dashboard/post_donation_screen.dart
+++ lib/src/screens/dashboard/post_donation_screen.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
+import 'main_navigation_screen.dart'; // Your bottom navigation
+import '../../services/cloudinary_service.dart'; //introduces cloudinary service for image upload
 
+class PostDonationScreen extends StatefulWidget {
+  const PostDonationScreen({super.key});
+
+  @override
+  State<PostDonationScreen> createState() => _PostDonationScreenState();
+}
