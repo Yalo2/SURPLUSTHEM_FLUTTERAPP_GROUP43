bc067891b4dad23749cb2497f96e89f52a276bf1
joshkwizera11 <joshngoboka1@gmail.com>
2026-07-20 15:52:42 +0300

    const

--- lib/src/services/cloudinary_service.dart
+++ lib/src/services/cloudinary_service.dart
@@ -2,3 +2,6 @@ import 'dart:io';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+Future<String?> uploadToCloudinary(File imageFile) async {
+  const cloudName = 'qzxfamih'; // replace with your actual Cloud Name
+  const uploadPreset = 'MAD_GROUP43'; // replace with your actual preset name
