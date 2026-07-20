2bde0ff234e87bd12a96eac22d6664d7585a16e8
joshkwizera11 <joshngoboka1@gmail.com>
2026-07-20 15:46:40 +0300

    imports

--- lib/src/routes/route_generator.dart
+++ lib/src/routes/route_generator.dart
@@ -1,2 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:surplusthem/src/routes/app_routes.dart';
+import 'package:surplusthem/src/screens/auth/login_screen.dart';
+import 'package:surplusthem/src/screens/auth/register_screen.dart';
