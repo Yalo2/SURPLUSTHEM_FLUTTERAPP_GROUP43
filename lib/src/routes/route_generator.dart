35ce2b93e64f0208f3d24085c2a6b9d8aba13c69
joshkwizera11 <joshngoboka1@gmail.com>
2026-07-20 15:48:07 +0300

    switch and case

--- lib/src/routes/route_generator.dart
+++ lib/src/routes/route_generator.dart
@@ -5,3 +5,13 @@ import 'package:surplusthem/src/screens/auth/register_screen.dart';
 import 'package:surplusthem/src/screens/auth/splash_screen.dart';
 import 'package:surplusthem/src/screens/dashboard/main_navigation_screen.dart';
 import 'package:surplusthem/src/screens/dashboard/item_detail_screen.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRoutes.splash:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+      case AppRoutes.login:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case AppRoutes.register:
+        return MaterialPageRoute(builder: (_) => const RegisterScreen());
