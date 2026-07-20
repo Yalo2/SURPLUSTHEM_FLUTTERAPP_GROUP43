575870ba6d6f20045644f3c159eef0c7fb23ec7d
joshkwizera11 <joshngoboka1@gmail.com>
2026-07-20 15:53:22 +0300

    final url

--- lib/src/services/cloudinary_service.dart
+++ lib/src/services/cloudinary_service.dart
@@ -5,3 +5,12 @@ import 'package:http/http.dart' as http;
 Future<String?> uploadToCloudinary(File imageFile) async {
   const cloudName = 'qzxfamih'; // replace with your actual Cloud Name
   const uploadPreset = 'MAD_GROUP43'; // replace with your actual preset name
+
+  final url = Uri.parse(
+    'https://api.cloudinary.com/v1_1/$cloudName/image/upload',
+  );
+  final request =
+      http.MultipartRequest('POST', url)
+        ..fields['upload_preset'] = uploadPreset
+        ..files.add(await http.MultipartFile.fromPath('file', imageFile.path));
+
