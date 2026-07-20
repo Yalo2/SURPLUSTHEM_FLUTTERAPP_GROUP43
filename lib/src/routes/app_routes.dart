317b9ecf80a60aeda70e1e452842a7788f56290d
joshkwizera11 <joshngoboka1@gmail.com>
2026-07-20 15:42:08 +0300

    ediited routes

--- lib/src/routes/app_routes.dart
+++ lib/src/routes/app_routes.dart
@@ -1,1 +1,10 @@
+class AppRoutes {
+  AppRoutes._();
+
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String mainNavigation = '/main';
+  static const String itemDetail = '/item-detail';
+}
 
