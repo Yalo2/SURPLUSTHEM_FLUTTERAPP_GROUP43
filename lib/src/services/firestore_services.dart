7d26dbd9ef9509c955cada0b25410b43c6edcbbb
joshkwizera11 <joshngoboka1@gmail.com>
2026-07-20 15:56:54 +0300

    user id

--- lib/src/services/firestore_services.dart
+++ lib/src/services/firestore_services.dart
@@ -1,3 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+class FirestoreService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Get current user ID
+  String get currentUserId => _auth.currentUser!.uid;
+
